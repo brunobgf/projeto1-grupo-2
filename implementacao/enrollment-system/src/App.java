bef8189d4aa2010d2f0e6d8ec69865d88506b4bb
brunobgf <brunobgf40@gmail.com>
2023-03-11 23:40:02 -0300

    feat: logic from university class moved to app and implementation of load courses and subjects

---
+++ implementacao/enrollment-system/src/App.java
@@ -0,0 +1,125 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    // #region AUXILIARY VARIABLES
+    private static Scanner keyboard = new Scanner(System.in, "UTF-8");
+    static University universityEnrollmentSystem = new University();
+    private static int keyAccess;
+    private static String path = "C:\\Users\\bruno\\OneDrive\\Documentos\\folder\\projeto1-grupo-2\\implementacao\\enrollment-system\\src";
+    // #endregion
+
+    // #region UTILS
+    public static void clear() {
+        System.out.print("/033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void pause() {
+        System.out.println("\nPress enter to continue.");
+        keyboard.nextLine();
+    }
+
+    // #endregion
+
+    // #region - MENUS
+    public static int menu() {
+        System.out.println();
+        System.out.println("Sistema de matriculas");
+        System.out.println("========================================");
+        System.out.println("Seja bem vindo! O que você gostaria de fazer?");
+        System.out.println("1 - Login");
+        System.out.println("0 - Sair");
+        System.out.print("Digite sua opção: ");
+        try {
+            int option = keyboard.nextInt();
+            keyboard.nextLine();
+            return option;
+        } catch (InputMismatchException ie) {
+            return -1;
+        }
+    }
+
+    // #region -LOGIN FLOW
+    public static void login() {
+        System.out.println("Digite seu usuário");
+        String user = keyboard.next();
+        System.out.println("Digite sua senha");
+        String password = keyboard.next();
+
+        keyAccess = University.getKeyAccess(user, password);
+
+        if (keyAccess == 0)
+            System.out.println("Usuário não encontrado!");
+    }
+
+    public static void userMenu() {
+        switch (University.getUser(keyAccess).getAccessLeve()) {
+            case 1:
+                optionsAccessLevelOne();
+                break;
+            case 2:
+                optionsAccessLevelTwo();
+                break;
+            default:
+                optionsAccessLevelAdm();
+        }
+    }
+
+    private static void optionsAccessLevelTwo() {
+
+    }
+
+    private static void optionsAccessLevelOne() {
+        System.out.println("\nOlá " + University.getUser(keyAccess).getUser() + ".\nO que deseja fazer?\n");
+        System.out.println("01. Visualizar lista de alunos");
+        int opc = keyboard.nextInt();
+
+        switch (opc) {
+            case 1:
+                University.getStudents();
+                break;
+        }
+    }
+
+    private static void optionsAccessLevelAdm() {
+        System.out.println("Incluir novo estudante");
+        int opc = keyboard.nextInt();
+
+        switch (opc) {
+            case 1:
+                University.addStudent();
+                break;
+        }
+
+    }
+    // #endregion
+
+    // #endregion
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in, "UTF-8");
+        University.loadSubjectData(path);
+        University.loadCourseData(path);
+        University.loadProfessorData(path);
+
+        int option;
+
+        do {
+            clear();
+            option = menu();
+            switch (option) {
+                case 1:
+                    login();
+                    userMenu();
+                    break;
+
+            }
+            pause();
+        } while (option != 0);
+
+        keyboard.close();
+
+    }
+
+}
--- implementacao/enrollment-system/src/University.java
+++ implementacao/enrollment-system/src/University.java
@@ -4,205 +4,212 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class University {
-
+	// #region - ATTRIBUTES
 	private static LinkedList<Course> courses = new LinkedList<Course>();
+	private static LinkedList<Subject> subjectList = new LinkedList<Subject>();
 	private static LinkedList<User> users = new LinkedList<User>();
-	private static Secretary secretary;
 	private static int keyAccess;
-	private static Scanner teclado = new Scanner(System.in, "UTF-8");
+	private static Scanner keyboard = new Scanner(System.in, "UTF-8");
+	// #endregion
 
-	public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in, "UTF-8");
-
-		//TESTE PQ NAO TEM DADOS INSERIDOS AINDA
-		LinkedList<Subject> listTest = new LinkedList<Subject>();
-		LinkedList<Subject> listTest2 = new LinkedList<Subject>();
-
-		String[] vetTest = {"Calculo", "A", "O", "20"};
-		Subject newSubject = new Subject(vetTest);
-		listTest.add(newSubject);
-		Course test = new Course("Engenharia de Software", listTest);
-		courses.add(test);
-
-		
-		String[] vetTest2 = {"Estatistica", "A", "O", "20"};
-		Subject newSubject2 = new Subject(vetTest2);
-		listTest2.add(newSubject2);
-		Course test2 = new Course("Administracao", listTest2);
-		courses.add(test2);
-        //FIM TESTE
-		
-		loadProfessorData("C:/Users/Cliente Vip Infomac/Desktop/PUC/LABPS/projeto1-grupo-2/implementacao/enrollment-system/src");
-		login();
-		userMenu();
-		teclado.close();
-    }
-
-	public static void login() {
-		System.out.println("Digite seu usuário");
-		String user = teclado.next();	
-		System.out.println("Digite sua senha");
-		String password = teclado.next();
-
-		keyAccess = users.stream()
-                    	 .filter(c -> c.getUser().equals(user))
-                         .filter(c -> c.getPassword().equals(password))
-                         .findFirst().get().getKeyAccess();
-
-		if (keyAccess == 0)
-		   System.out.println("Usuário não encontrado!");
+	// #region UTILS
+	public static int getKeyAccess(String user, String password) {
+		return keyAccess = users.stream()
+				.filter(c -> c.getUser().equals(user))
+				.filter(c -> c.getPassword().equals(password))
+				.findFirst().get().getKeyAccess();
 	}
 
-	public static void userMenu() {
-		switch (getUser(keyAccess).getAccessLeve()){
-	       case 1: optionsAccessLevelOne();
-		   		   break;
-	       case 2: optionsAccessLevelTwo();
-		  		   break;
-		   default: optionsAccessLevelAdm();
-		}
-		
-	}
-
-	private static User getUser(int keyAccess){
+	static User getUser(int keyAccess) {
 		return users.stream()
-                    .filter(c -> c.getKeyAccess() == keyAccess)
-					.findFirst().get();
+				.filter(c -> c.getKeyAccess() == keyAccess)
+				.findFirst().get();
 	}
 
-	private static void optionsAccessLevelTwo() {
-		
-	}
+	// #endregion
 
-	private static void optionsAccessLevelOne() {
-		System.out.println("\nOlá " + getUser(keyAccess).getUser() + ".\nO que deseja fazer?\n");
-		System.out.println("01. Visualizar lista de alunos");
-		int opc = teclado.nextInt();
-
-		switch (opc){
-			case 1: getStudents();
-					break;
-		} 
-	}
-
-	private static void optionsAccessLevelAdm() {
-		System.out.println("Incluir novo estudante");
-		int opc = teclado.nextInt();
-
-		switch (opc){
-			case 1: addStudent();
-					break;
-		} 
-		 
-	}
-
-	private static void addStudent() {		
+	// #region - ACTIONS
+	static void addStudent() {
 		String[] dados = new String[2];
 		LinkedList<Course> listCourses = new LinkedList<Course>();
 
 		System.out.println("Digite o nome do aluno");
-		dados[0] = teclado.next();
+		dados[0] = keyboard.next();
 
 		System.out.println("Digite a senha de acesso do aluno");
-		dados[1] = teclado.next();
+		dados[1] = keyboard.next();
 
 		System.out.println("Digite o nome do curso em que o estudade está matriculado");
-		String nameCourse= teclado.next();
+		String nameCourse = keyboard.next();
 
 		int opc = -1;
 		LinkedList<Subject> subjects = new LinkedList<Subject>();
 
-		while (opc != 0){
+		while (opc != 0) {
 			System.out.println("Escolha a disciplina. Para parar, digite 0");
 			getCourse(nameCourse).toString();
-			opc = teclado.nextInt();
+			opc = keyboard.nextInt();
 
 			if (opc != 0)
-			subjects.add(getCourse(nameCourse).getSubjectsList().get(opc-1));
+				subjects.add(getCourse(nameCourse).getSubjectsList().get(opc - 1));
 		}
 
 		listCourses.add(new Course(nameCourse, subjects));
 		User newStudent = new Student(dados, listCourses);
-		users.add(newStudent);		
+		users.add(newStudent);
 	}
 
 	private static void addProfessor() {
-		
+
 	}
 
 	private static void addSubject() {
-		
+
 	}
 
 	private static void enrollStudent(int key) {
 		System.out.println("Digite o curso");
-		String course = teclado.next();
+		String course = keyboard.next();
 
 		System.out.println("Digite a disciplina");
-		String subject = teclado.next();
+		String subject = keyboard.next();
 
 		Subject newSubject;
 
 		newSubject = getUser(key).getCourse(course).subjectAvailable(subject);
 
-		if (newSubject != null) 
-		   getUser(key).getCourse(course).newSubject(newSubject);	
+		if (newSubject != null)
+			getUser(key).getCourse(course).newSubject(newSubject);
 	}
 
-	private static void getStudents() {
+	static void getStudents() {
 		System.out.println("Digite o curso");
-		String course = teclado.next();
+		String course = keyboard.next();
 
 		System.out.println("Digite a disciplina");
-		String subject = teclado.nextLine();
+		String subject = keyboard.nextLine();
 
-	 users.stream()
-	      .filter(c -> c.getAccessLeve() == 3)
-		  .filter(c -> c.getCourse(course) != null)
-		  .filter(c -> c.getCourse(course).subjectAvailable(subject) != null)
-		  .forEach(System.out::println);
+		users.stream()
+				.filter(c -> c.getAccessLeve() == 3)
+				.filter(c -> c.getCourse(course) != null)
+				.filter(c -> c.getCourse(course).subjectAvailable(subject) != null)
+				.forEach(System.out::println);
+	}
+	// #endregion
+
+	// #region - FILE READERS
+
+	public static void loadCourseData(String coursePath) {
+		try {
+			Path path = Paths.get(coursePath.concat("/Course.txt"));
+			Scanner sc = new Scanner(path, "UTF-8");
+			while (sc.hasNextLine()) {
+				String row = sc.nextLine();
+
+				addCourseToList(row);
+			}
+			sc.close();
+		} catch (IOException io) {
+			System.out.println("Error at opening the file");
+		}
 	}
 
-	public static void loadProfessorData(String caminho) {
-        try {
-            Path path = Paths.get(caminho.concat("/Professor.txt"));
-            Scanner sc = new Scanner(path, "UTF-8");
-            while (sc.hasNextLine()) {
-                String linha = sc.nextLine();
+	public static void addCourseToList(String row) {
+		String[] rowSplit = row.split(";");
+		String courseName = rowSplit[0];
 
-                loadProfessor(linha);
-            }
-            sc.close();
-        } catch (IOException io) {
-            System.out.println("Erro ao abrir arquivo");
-        }
-    }
+		LinkedList<Subject> subjectsFromCourse = new LinkedList<Subject>();
 
-	public static Course getCourse(String name){
-		return courses.stream()
-						   .filter(c -> c.getName().equals(name))
-						   .findFirst().get();
-	}
-
-	public static void loadProfessor(String dados){
-		String[] professor = dados.split(";");
-		String[] courses = professor[5].split(",");
-		LinkedList<Course> listCourses = new LinkedList<Course>();
-		LinkedList<Subject> listSubjects = new LinkedList<Subject>();
-
-		for (int i = 0; i < courses.length; i++){
-		   String[] vetAux = courses[i].split("=");
-		   String nameCourse = vetAux[0];
-		   String[] subjects = vetAux[1].split("/");
-
-		   for (int y = 0; y < subjects.length; y++)
-		     listSubjects.add(getCourse(nameCourse).getSubject(subjects[y]));
-
-		   listCourses.add(new Course(nameCourse, listSubjects));
+		for (int i = 1; i < rowSplit.length; i++) {
+			if (rowSplit[i].equals(getSubject(rowSplit[i]).getName())) {
+				subjectsFromCourse.add(getSubject(rowSplit[i]));
+			}
 		}
 
-		User newProfessor = new Professor(professor, listCourses);
+		Course course = new Course(courseName, subjectsFromCourse);
+
+		courses.add(course);
+	}
+
+	public static Subject getSubject(String subjectName) {
+
+		return subjectList.stream()
+				.filter(c -> c.getName().equals(subjectName))
+				.findFirst().get();
+
+	}
+
+	public static void loadSubjectData(String subjectPath) {
+		try {
+			Path path = Paths.get(subjectPath.concat("/Subject.txt"));
+			Scanner sc = new Scanner(path, "UTF-8");
+			while (sc.hasNextLine()) {
+				String row = sc.nextLine();
+
+				addSubjectToList(row);
+			}
+			sc.close();
+		} catch (IOException io) {
+			System.out.println("Error at opening the file");
+		}
+	}
+
+	public static void addSubjectToList(String row) {
+		String[] rowSplit = row.split(";");
+		Subject subject = new Subject(rowSplit);
+
+		subjectList.add(subject);
+
+	}
+
+	public static void loadProfessorData(String professorPath) {
+		try {
+			Path path = Paths.get(professorPath.concat("/Professor.txt"));
+			Scanner sc = new Scanner(path, "UTF-8");
+			while (sc.hasNextLine()) {
+				String row = sc.nextLine();
+
+				loadProfessor(row);
+			}
+			sc.close();
+		} catch (IOException io) {
+			System.out.println("Error at opening the file");
+		}
+	}
+
+	public static Course getCourse(String name) {
+		return courses.stream()
+				.filter(c -> c.getName().equals(name))
+				.findFirst().get();
+	}
+
+	public static void loadProfessor(String dados) {
+		String[] professor = dados.split(";");
+		String[] courses = professor[5].split(",");
+		User newProfessor = new Professor(professor, loadWorkingCourses(courses));
 		users.add(newProfessor);
 	}
+
+	private static LinkedList<Course> loadWorkingCourses(String[] courses) {
+		LinkedList<Subject> listSubjects = new LinkedList<Subject>();
+		LinkedList<Course> listCourses = new LinkedList<Course>();
+
+		for (int i = 0; i < courses.length; i++) {
+			String[] vetAux = courses[i].split("=");
+			String nameCourse = vetAux[0];
+			String[] subjects = vetAux[1].split("/");
+
+			for (int y = 0; y < subjects.length; y++) {
+				Subject subject = getCourse(nameCourse).getSubject(subjects[y]);
+				listSubjects.add(subject);
+			}
+
+			listCourses.add(new Course(nameCourse, listSubjects));
+		}
+
+		return listCourses;
+	}
+
+	// #endregion
 
 }
